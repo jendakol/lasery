fc448058171994965ea6e80d5143aa93cedaccb4
Jan Kolena <jendakolena@gmail.com>
2021-04-03 19:20:24 +0200

    Receiver: Stabilized
    Redesigned to ESP32
    Used Tasker

--- receiver/src/main.cpp
+++ receiver/src/main.cpp
@@ -1,3 +1,6 @@
+#pragma clang diagnostic push
+#pragma ide diagnostic ignored "cert-err58-cpp"
+
 #include <Arduino.h>
 
 #include "../../common/Constants.h"
@@ -5,8 +8,11 @@
 #include <ESPAsyncWebServer.h>
 #include <DNSServer.h>
 #include <FS.h>
-
+#include <SPIFFS.h>
 #include <ArduinoJson.h>
+#include <set>
+
+#include <Tasker.h>
 
 #define DNS_PORT 53
 #define STATIC_FILES_PREFIX "/w"
@@ -16,81 +22,103 @@ AsyncWebSocket wsSensors(SENSOR_WS_PATH);
 AsyncWebSocket wsFrontend(FRONTEND_WS_PATH);
 DNSServer dnsServer;
 
-static FS FileSystem = SPIFFS;
+#define FileSystem SPIFFS
 
 #define STATE_UNARMED 0
 #define STATE_ARMED 1
 #define STATE_ALERTING 2
 
+#define CLIENT_PONG_TIMEOUT (PING_EVERY + 500)
+
 // TODO change back to STATE_UNARMED;
 int appState = STATE_ARMED;
 unsigned long lastReport = 0;
 
+unsigned long pingNo = 0;
+
 std::set<uint32_t> alerting_ids;
+std::map<uint32_t, uint32_t> lastPongs;
 
 void reportStateToFrontend();
 
-void onTextMessage(AsyncWebSocketClient *client, uint8_t *data, const size_t len) {
-    if (len > 90) {
-        Serial.println(F("Received some too-long shit, skipping"));
-        return;
-    }
+void updateStatus() {
+    if (appState != STATE_UNARMED) appState = alerting_ids.empty() ? STATE_ARMED : STATE_ALERTING;
+}
 
-//    Serial.print(F("Websocket data received: "));
-//
-//    for (uint i = 0; i < len; i++) {
-//        Serial.print((char) data[i]);
-//    }
-//    Serial.println();
+void clearClients() {
+    wsSensors.cleanupClients();
 
-    StaticJsonDocument<100> requestJson;
-    deserializeJson(requestJson, data, len);
+    const unsigned long now = millis();
 
-    if (requestJson["type"] == "handshake") {
-        StaticJsonDocument<20> responseJson;
+    for (auto client: wsSensors.getClients()) {
+        const uint32_t lastPong = lastPongs[client->id()];
 
-        responseJson["status"] = "ok";
-
-        char output[20];
-        size_t l = serializeJson(responseJson, output);
-
-        client->text(output, l);
-        return;
-    }
-
-    if (requestJson["type"] == "alert") {
-        if (requestJson["status"] == "alert") {
-            alerting_ids.insert(client->id());
-        } else if (requestJson["status"] == "ok") {
-            alerting_ids.erase(client->id());
-        };
-
-        if (appState != STATE_UNARMED) appState = alerting_ids.empty() ? STATE_ARMED : STATE_ALERTING;
-
-        StaticJsonDocument<20> responseJson;
-
-        responseJson["status"] = "ok";
-
-        char output[20];
-        size_t l = serializeJson(responseJson, output);
-
-        client->text(output, l);
-        return;
+        if (duration(now, lastPong) > CLIENT_PONG_TIMEOUT) {
+            Serial.printf("Kicking unresponsive client %d\n", client->id());
+            client->client()->close(true);
+            lastPongs.erase(client->id());
+        }
     }
 }
 
-void onSensorWsEvent(AsyncWebSocket *server, AsyncWebSocketClient *client, AwsEventType type, void *arg, uint8_t *data, const size_t len) {
+void onTextMessage(AsyncWebSocketClient *client, uint8_t *data, const size_t len) {
+    StaticJsonDocument<50> requestJson;
+    deserializeJson(requestJson, data, len);
+
+    const String type = requestJson["type"];
+
+    if (type == "alert") {
+        const String status = requestJson["status"];
+
+        Serial.printf("Received alert info from sensor %d: %s\n", client->id(), status.c_str());
+
+        if (status == "alert") {
+            alerting_ids.insert(client->id());
+        } else if (status == "alert-ok") {
+            alerting_ids.erase(client->id());
+        }
+
+        updateStatus();
+
+        StaticJsonDocument<30> responseJson;
+
+        responseJson["status"] = status;
+
+        char output[30];
+        size_t l = serializeJson(responseJson, output);
+
+        client->text(output, l);
+        return;
+    }
+
+    Serial.printf("Received unknown type: '%s'\n", type.c_str());
+}
+
+void onSensorWsEvent(__unused AsyncWebSocket *server, AsyncWebSocketClient *client,
+                     AwsEventType type, __unused void *arg, uint8_t *data, const size_t len) {
+    const unsigned long now = millis();
+
     if (type == WS_EVT_CONNECT) {
-        Serial.println(F("Sensor client connection received"));
+        client->keepAlivePeriod(30000);
+        Serial.printf("Sensor client %d connection received\n", client->id());
+        lastPongs.insert(std::pair<uint32_t, uint32_t>(client->id(), now));
+        const String &string = String(pingNo);
+        client->ping((uint8_t *) string.c_str(), string.length());
     } else if (type == WS_EVT_DISCONNECT) {
-        Serial.println(F("Sensor client disconnected"));
+        Serial.printf("Sensor client %d disconnected\n", client->id());
+        alerting_ids.erase(client->id());
+        lastPongs.erase(client->id());
+    } else if (type == WS_EVT_PONG) {
+        lastPongs[client->id()] = now;
     } else if (type == WS_EVT_DATA) {
+        lastPongs[client->id()] = now;
         onTextMessage(client, data, len);
         reportStateToFrontend();
     }
 }
 
-void onFrontendWsEvent(AsyncWebSocket *s, AsyncWebSocketClient *client, AwsEventType type, void *arg, uint8_t *data, const size_t len) {
+void onFrontendWsEvent(__unused AsyncWebSocket *s, __unused AsyncWebSocketClient *client,
+                       AwsEventType type, __unused void *arg, uint8_t *data, const size_t len) {
     if (type == WS_EVT_CONNECT) {
         Serial.println(F("Frontend client connection received"));
     } else if (type == WS_EVT_DISCONNECT) {
@@ -150,17 +178,17 @@ void reportStateToFrontend() {
 
 String getContentType(const String &filename) {
     if (filename.endsWith(F(".htm"))) return F("text/html");
-    else if (filename.endsWith(F(".html"))) return F("text/html");
-    else if (filename.endsWith(F(".css"))) return F("text/css");
-    else if (filename.endsWith(F(".js"))) return F("application/javascript");
-    else if (filename.endsWith(F(".png"))) return F("image/png");
-    else if (filename.endsWith(F(".gif"))) return F("image/gif");
-    else if (filename.endsWith(F(".jpg"))) return F("image/jpeg");
-    else if (filename.endsWith(F(".ico"))) return F("image/x-icon");
-    else if (filename.endsWith(F(".xml"))) return F("text/xml");
-    else if (filename.endsWith(F(".pdf"))) return F("application/x-pdf");
-    else if (filename.endsWith(F(".zip"))) return F("application/x-zip");
-    else if (filename.endsWith(F(".gz"))) return F("application/x-gzip");
+    if (filename.endsWith(F(".html"))) return F("text/html");
+    if (filename.endsWith(F(".css"))) return F("text/css");
+    if (filename.endsWith(F(".js"))) return F("application/javascript");
+    if (filename.endsWith(F(".png"))) return F("image/png");
+    if (filename.endsWith(F(".gif"))) return F("image/gif");
+    if (filename.endsWith(F(".jpg"))) return F("image/jpeg");
+    if (filename.endsWith(F(".ico"))) return F("image/x-icon");
+    if (filename.endsWith(F(".xml"))) return F("text/xml");
+    if (filename.endsWith(F(".pdf"))) return F("application/x-pdf");
+    if (filename.endsWith(F(".zip"))) return F("application/x-zip");
+    if (filename.endsWith(F(".gz"))) return F("application/x-gzip");
 
     return F("text/plain");
 }
@@ -229,14 +257,15 @@ void createAP() {
     Serial.print(F("Configuring access point with SSID "));
     Serial.println(WIFI_SSID);
 
-    WiFi.softAPConfig(apIP, apIP, netMsk);
     WiFi.softAP(WIFI_SSID, WIFI_PASSWORD);
+    Tasker::sleep(100);
+    WiFi.softAPConfig(apIP, apIP, netMsk);
 
     /* Setup the DNS server redirecting all the domains to the apIP */
     dnsServer.setErrorReplyCode(DNSReplyCode::NoError);
     dnsServer.start(DNS_PORT, "*", apIP);
 
-    delay(500);
+    Tasker::sleep(500);
     Serial.print(F("AP IP address: "));
     Serial.println(WiFi.softAPIP());
 
@@ -244,49 +273,68 @@ void createAP() {
 }
 
 void setup() {
-    Serial.begin(9600);
+    Serial.begin(115200);
     FileSystem.begin();
 
-    createAP();
+    NetworkTasker.once("network-setup", [] {
+        createAP();
 
-    webServer.onNotFound([](AsyncWebServerRequest *request) {
-        if (handleStaticFile(request)) return;
+        webServer.onNotFound([](AsyncWebServerRequest *request) {
+            if (handleStaticFile(request)) return;
+            request->send(404);
+        });
 
-        request->send(404);
+        webServer.addHandler(&wsSensors);
+        webServer.addHandler(&wsFrontend);
+        wsSensors.onEvent(onSensorWsEvent);
+        wsFrontend.onEvent(onFrontendWsEvent);
+
+        webServer.begin();
+        Serial.println(F("HTTP server started"));
     });
 
-    webServer.addHandler(&wsSensors);
-    webServer.addHandler(&wsFrontend);
-    wsSensors.onEvent(onSensorWsEvent);
-    wsFrontend.onEvent(onFrontendWsEvent);
+    DefaultTasker.loop("DNS-handle", [] {
+        dnsServer.processNextRequest();
+    });
 
-    webServer.begin();
-    Serial.println(F("HTTP server started"));
+    DefaultTasker.loopEvery("SendPing", PING_EVERY, [] {
+        clearClients();
+
+        const String &string = String(++pingNo);
+        const char *data = string.c_str();
+        wsSensors.pingAll((uint8_t *) data, string.length());
+    });
+
+    DefaultTasker.loopEvery("PrintStatus", 10, [] {
+        updateStatus();
+
+        const unsigned long now = millis();
+
+        if (appState == STATE_ALERTING && (duration(now, lastReport) > 500)) {
+            lastReport = now;
+            Serial.print(F("Connected clients: "));
+            Serial.print(wsSensors.getClients().length());
+            Serial.println(F(" State: ALERTING"));
+        } else if (duration(now, lastReport) > 2000) {
+            lastReport = now;
+            Serial.print(F("Connected clients: "));
+            Serial.print(wsSensors.getClients().length());
+            Serial.print(F(" State: "));
+
+            switch (appState) {
+                case STATE_UNARMED:
+                    Serial.println("UNARMED");
+                    break;
+                case STATE_ARMED:
+                    Serial.println("ARMED");
+                    break;
+            }
+        }
+    });
 }
 
 void loop() {
-    dnsServer.processNextRequest();
-    wsSensors.cleanupClients();
-
-    if (appState == STATE_ALERTING) {
-        lastReport = millis();
-        Serial.print(F("Connected clients: "));
-        Serial.print(wsSensors.getClients().length());
-        Serial.println(F(" State: ALERTING"));
-        delay(100);
-    } else if (millis() - lastReport > 1000) {
-        lastReport = millis();
-        Serial.print(F("Connected clients: "));
-        Serial.print(wsSensors.getClients().length());
-        Serial.print(F(" State: "));
-
-        switch (appState) {
-            case STATE_UNARMED:
-                Serial.println("UNARMED");
-                break;
-            case STATE_ARMED:
-                Serial.println("ARMED");
-                break;
-        }
-    }
+    // no-op, handling by Tasker
 }
+
+#pragma clang diagnostic pop
